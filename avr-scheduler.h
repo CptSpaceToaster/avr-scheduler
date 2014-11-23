b66f97f1a7384de1f27f1991b273558a9c4a45ba
CptSpaceToaster <Lugia978@yahoo.com>
2014-11-23 15:06:05 -0500

    updated some documentation

--- avr-scheduler.h
+++ avr-scheduler.h
@@ -3,6 +3,21 @@
  *
  * Created: 11/22/2014 2:44:29 PM
  * Author: CaptainSpaceToaster
+ *
+ * A millisecond resolution scheduler that holds and maintains a FIFO-doubly-linked-list of event_t structures inside 
+ * of a schedule_t structure.  (very queue like, but the list can prioritize new events)
+ * 
+ * - Define a schedule_t, and initalize it with a call to new_schedule()
+ *
+ * - Fill the schedule_t by calling schedule_push() or schedule_insert(), however note that you can severely severely 
+ *   impair the sorting algorithm if you push an event that's not in the right order.
+ *
+ * - Look at the last item in the schedule by calling schedule_peek() (this would be the "next item to handle")
+ *
+ * - Remove the last item from the schedule by calling schedule_pop() (this would be used to remove and handle the 
+ *   next scheduled item)
+ *
+ * Create issues on github, and feel free to ask for more clarification
  */ 
 #ifndef _SCHEDULE_H_
 #define _SCHEDULE_H_
@@ -15,6 +30,7 @@
 #define SCHEDULE_TIME_CEILING       60000
 #define GET_EVENT_TIME(EVENT_T)     ((uint16_t)((EVENT_T.ms_duration + EVENT_T.ms_time)%SCHEDULE_TIME_CEILING))
 
+/* Nodes of the doubly-linked-list */
 typedef struct event {
 	uint16_t ms_duration;
 	uint16_t ms_time;
@@ -23,19 +39,20 @@ typedef struct event {
 	struct event *previous;
 } event_t;
 
+/* Container structure for the list */
 typedef struct {
 	event_t *first_event;               // Position of first event (root)
 	event_t *last_event;                // Position of last event
 	uint8_t size;                       // Number of scheduled events
 } schedule_t;
 
-#define SCHEDULE_NOT_FOUND      0x01    //This can't be changed, as 1 equates to true, and is_empty() returns this error
+#define SCHEDULE_NOT_FOUND      0x01    //Don't change this, as 1 equates to true, and is_empty() returns this as an error
 #define SCHEDULE_OVERFLOW       0x02
 #define TIME_OVERFLOW           0x03
 #define DURATION_TOO_LONG       0x04
 #define NULL_EVENT_ERROR        0x05
 
-/* Creates an event_t in memory, and returns its pointer. */
+/* Creates an event_t in memory, and returns its pointer. You shouldn't need this unless attempting to "push" events onto a schedule */
 event_t* new_event(uint16_t duration, uint16_t current_time, uint8_t new_id);
 
 /* Creates a schedule_t in memory, and returns its pointer.  Sets the first and last element to NULL nodes, and size value to zero. */
